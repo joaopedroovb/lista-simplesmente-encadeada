ea3b8f00c50e721d91813d5e98bfd8e7af5f9a66
João Pedro <olliveiravb@gmail.com>
2022-08-08 20:20:40 -0300

    Create LSE.java

---
+++ LSE.java
@@ -0,0 +1,27 @@
+package lse;
+
+public class LSE {
+
+    public static void main(String[] args) {
+        Lista l = new Lista();
+        l.insereInicio(5);
+        l.imprime();
+        
+        l.insereInicio(7);
+        l.imprime();
+        
+        l.insereFim(10);
+        l.imprime();
+        
+        l.inserePescoco(8);
+        l.imprime();
+        
+        l.inserePertoDoFim(11);
+        l.imprime();
+        
+        l.inserePenultimo(20);
+        l.imprime();
+        
+        
+    } 
+}
